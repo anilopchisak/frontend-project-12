0545b24ce3a39ef59db462b2dbea7617287a6f1e
Polina Kasich <102850763+anilopchisak@users.noreply.github.com>
2025-08-11 17:51:46 +0500

    add rollbar

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -4,18 +4,28 @@ import Header from '../widgets/header/Header.jsx'
 import AppRouter from "./providers/router/AppRouter.jsx"
 import ToastifyContainer from "../shared/ui/toastifyContainer/ToastifyContainer.jsx";
 import ModalWindow from "../shared/ui/modal/ModalWindow.jsx";
+import { Provider, ErrorBoundary } from '@rollbar/react'
+
+const rollbarConfig = {
+    accessToken: import.meta.env.ACCESS_TOKEN_ROLLBAR,
+    environment: 'testenv',
+}
 
 const App = () => {
-  return (
-    <>
-        <Header />
-        <main id='content'>
-            <AppRouter />
-        </main>
-        <ToastifyContainer />
-        <ModalWindow />
-    </>
-  )
+    return (
+      <Provider config={rollbarConfig}>
+          <ErrorBoundary>
+              <>
+                  <Header />
+                  <main id='content'>
+                      <AppRouter />
+                  </main>
+                  <ToastifyContainer />
+                  <ModalWindow />
+              </>
+          </ErrorBoundary>
+      </Provider>
+    )
 }
 
 export default App
